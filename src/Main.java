c8a3a821425672c4c9792663b07cb6688e978c2f
TinhNguyen97 <princetinkie97@gmail.com>
2022-01-04 10:51:42 +0700

    nd

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        float f = 10.5f;
+        double d = 10.5;
+        boolean b = true;
+        char c = 'c';
+        String s = "Ha Noi";
+        System.out.println("a= "+a);
+        System.out.println("f= "+f);
+        System.out.println("d= "+d);
+        System.out.println("b= "+b);
+        System.out.println("c= "+c);
+    }
+}
